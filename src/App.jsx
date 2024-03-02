8bfd33d1f7411ee2cfa517aa09d3ff00195dcac6
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-03-02 13:44:06 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import bg from "../src/assets/bg-1.jpg";
+import AboutUs from "./components/about_us";
 
 import Header from "./components/header";
 import Intro from "./components/intro";
@@ -10,15 +11,19 @@ function App() {
       <div className="w-screen max-w-full">
         <div
           className="min-h-full h-[1090px] min-w-full bg-cover"
-          style={{ backgroundImage: `url(${bg})`}}
+          style={{ backgroundImage: `url(${bg})` }}
         >
           <Header />
           <Intro />
         </div>
 
-        <MintNow />
+        <div>
+          <div className="sm:h-[900px] h-[1043px] lg:h-[800px] ">
+            <AboutUs />
+          </div>
+          <MintNow />
+        </div>
       </div>
-
     </>
   );
 }
