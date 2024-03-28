3b8a8deac00ce80e42a0070b37e4848fbd3f54a3
PhongThanh0907 <phong.thanh971997@gmail.com>
2024-03-28 20:16:37 +0700

    update code

--- src/components/join_us/index.jsx
+++ src/components/join_us/index.jsx
@@ -1,11 +1,11 @@
 import Bg from "../../assets/mint-now/bgJoinUs.jpg";
-import BG01 from '../../assets/mint-now/bg01.png'
-import BG02 from '../../assets/mint-now/bg02.png'
-import BG03 from '../../assets/mint-now/bg03.png'
+import BG01 from "../../assets/mint-now/bg01.png";
+import BG02 from "../../assets/mint-now/bg02.png";
+import BG03 from "../../assets/mint-now/bg03.png";
 
 const JoinUs = () => {
   return (
-    <div>
+    <div className="relative">
       <div className="lg:max-w-7xl px-4 lg:px-0 mx-auto">
         <h4
           style={{
@@ -17,11 +17,24 @@ const JoinUs = () => {
         </h4>
 
         <div className="relative h-[507px] w-full my-16">
-          <img className="absolute h-[829px] w-[841px] z-10 -top-[155px] right-0" style={{
-            transform: "matrix(-1, 0, 0, 1, 0, 0)"
-          }} src={BG01} alt="" />
-          <img className="absolute z-10 h-[277px] w-[277px] -top-[58px] left-[66px]" src={BG02} alt="" />
-          <img className="absolute z-10 h-[244px] w-[244px] -right-[66px] -top-[164px]" src={BG03} alt="" />
+          <img
+            className="absolute h-[829px] w-[841px] z-10 -top-[155px] right-0"
+            style={{
+              transform: "matrix(-1, 0, 0, 1, 0, 0)",
+            }}
+            src={BG01}
+            alt=""
+          />
+          <img
+            className="absolute z-10 h-[277px] w-[277px] -top-[58px] left-[66px]"
+            src={BG02}
+            alt=""
+          />
+          <img
+            className="absolute z-10 h-[244px] w-[244px] -right-[66px] -top-[164px]"
+            src={BG03}
+            alt=""
+          />
           <img className="w-full h-full absolute inset-0" src={Bg} alt="" />
           <div className="absolute bottom-16 left-10">
             <h4
@@ -45,16 +58,44 @@ const JoinUs = () => {
               stay tuned for any news.
             </p>
             <div className="flex gap-2">
-              <button style={{letterSpacing: "-1px", fontWeight: "700", padding: "5px 20px"}} className="text-[24px] leading-[35px]">Join Discord</button>
-              <button style={{letterSpacing: "-1px", fontWeight: "700", padding: "5px 20px", backgroundColor: "transparent", border: "1px solid #789D7C", color: "#789D7C" }} className="text-[24px] leading-[35px]">Twitter</button>
+              <button
+                style={{
+                  letterSpacing: "-1px",
+                  fontWeight: "700",
+                  padding: "5px 20px",
+                }}
+                className="text-[24px] leading-[35px]"
+              >
+                Join Discord
+              </button>
+              <button
+                style={{
+                  letterSpacing: "-1px",
+                  fontWeight: "700",
+                  padding: "5px 20px",
+                  backgroundColor: "transparent",
+                  border: "1px solid #789D7C",
+                  color: "#789D7C",
+                }}
+                className="text-[24px] leading-[35px]"
+              >
+                Twitter
+              </button>
             </div>
           </div>
         </div>
       </div>
+      <div
+        style={{
+          right: "-400px",
+          bottom: "-80px",
+          backgroundColor: "#11E2E6",
+          filter: "blur(150px)",
+        }}
+        className="w-[523px] h-[358px] absolute opacity-70"
+      ></div>
     </div>
   );
 };
 
 export default JoinUs;
-
-/* image_2024-02-22_16-41-30 (2)-Photoroom 4 */
