db3c2afc9cadb5546d8b5ff6879b0639f7a7dcd1
PhongThanh0907 <phong.thanh971997@gmail.com>
2024-03-31 16:41:32 +0700

    update code

--- src/components/marketplace/index.jsx
+++ src/components/marketplace/index.jsx
@@ -1,9 +1,18 @@
 import SliderCustom from "../slider";
+import BG from '../../assets/nois2.png'
 
 const Marketplace = () => {
   return (
-    <div className="">
-      <div className="max-w-7xl mx-auto px-4 lg:px-0">
+    <div className=" relative h-full">
+      <div className="absolute inset-0 h-[94%] -bottom-14 m-auto w-full z-10">
+        <img
+          src={BG}
+          alt=""
+          className="object-cover w-full h-full"
+        />
+
+      </div>
+      <div className="max-w-7xl mx-auto px-4 lg:px-0 relative z-0">
         <h3 className="title mb-20 hidden lg:block">Marketplace</h3>
         <h3
           style={{
