f1639e9ce2c62516ae8f06c7d026740bbbab4e4a
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-04-13 11:04:18 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
 
 function App() {
-
   useEffect(() => {
     setTimeout(() => {
       window.location.href = "https://uniquiz.online/";
-    }, 3000)
-  }, [])
+    }, 3000);
+  }, []);
 
   return (
     <div className="h-screen !text-white w-screen flex justify-center items-center">
       <p className="!text-white">
-        Xin vui lòng đợi trong giây lát, bạn đang được chuyển hướng... <br />
         <span className="flex justify-center">
-          Vui lòng không tắt trang hoặc trình duyệt !
+          Xin vui lòng đợi trong giây lát, bạn đang được chuyển hướng...
+        </span>
+        <span className="flex justify-center">
+          Lưu ý: Không tắt trang hoặc trình duyệt !
         </span>
       </p>
     </div>
