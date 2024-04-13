23ee85ef2e1e83c8590d510a827501d825027810
June Nghia <nguyenminhtrungnghia@gmail.com>
2024-04-13 10:59:25 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,22 @@
-import Footer from "./components/footer";
-
-import Header from "./components/header";
-import Intro from "./components/intro";
-import GamePlay from "./components/game_play";
-import RoadMap from "./components/roadmap";
-import MintNow from "./components/mint_now";
-import GetDino from "./components/get_dino";
-import AboutUs from "./components/about_us";
-import Marketplace from "./components/marketplace";
-import JoinUs from "./components/join_us";
+import { useEffect } from "react";
 
 function App() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = "https://uniquiz.online/";
+    }, 3000)
+  }, [])
+
   return (
-    <>
-      <div className="w-screen overflow-hidden relative">
-        {/* <div className="fixed top-0 h-screen w-screen">
-        <img className="h-full w-full object-cover" src={BG} alt="" />
-        </div> */}
-        <div className="relative z-0">
-        <Header />
-        <div id="home">
-          <Intro />
-        </div>
-
-         <div className="lg:mt-40 mt-20">
-          <GetDino />
-        </div>
-
-        <div id="about-us">
-          <div className="my-16">
-            <AboutUs />
-          </div>
-        </div>
-
-        <div id="marketplace">
-          <Marketplace />
-        </div>
-
-        <div id="game_play" className="lg:mt-40 mt-28">
-          <GamePlay />
-        </div>
-
-        <div id="mint-now">
-          <MintNow />
-        </div>
-
-        <div id="roadmap">
-          <RoadMap />
-        </div>
-        <div id="join_us">
-          <JoinUs />
-        </div> 
-
-        <div id="footer">
-          <Footer />
-        </div> 
-        </div>
-     
-      </div>
-    </>
+    <div className="h-screen !text-white w-screen flex justify-center items-center">
+      <p className="!text-white">
+        Xin vui lòng đợi trong giây lát, bạn đang được chuyển hướng... <br />
+        <span className="flex justify-center">
+          Vui lòng không tắt trang hoặc trình duyệt !
+        </span>
+      </p>
+    </div>
   );
 }
 
